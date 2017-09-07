aded5a4f1f999db2d2856b25a4c820098a491d19
saturnon <30634590+saturnon@users.noreply.github.com>
2017-09-07 11:57:35 -0300

    Add files via upload

---
+++ sensores/Constantes.h
@@ -0,0 +1,21 @@
+
+//Configuração para os pinos de sensor de oponente para input
+TRISB6_bit = 1;
+TRISA7_bit = 1;
+TRISB5_bit = 1;
+TRISB4_bit = 1;
+TRISB3_bit = 1;
+TRISA3_bit = 1;
+TRISA6_bit = 1;
+
+//Declarando os pinos para seu estado default de low
+
+RB6_bit = 0;
+RA7_bit = 0;
+RB5_bit = 0;
+RB4_bit = 0;
+RB3_bit = 0;
+RA6_bit = 0;
+RA3_bit = 0;
+
+float SensorReading();
